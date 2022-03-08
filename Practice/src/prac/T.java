51112f6be8fc7f283662aed11226213e4c65bb4d
vsonkusare1020 <96275813+vsonkusare1020@users.noreply.github.com>
2022-03-09 00:01:35 +0530

    update in T.java

--- Practice/src/prac/T.java
+++ Practice/src/prac/T.java
@@ -3,6 +3,7 @@ package prac;
 public class T {
 	public void m1(Object a) {
 		System.out.println("obj");
+		System.out.println("obj");
 	}
 	public void m1(String s) {
 		System.out.println("String");
